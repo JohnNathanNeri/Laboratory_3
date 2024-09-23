a6b6cea9a58f07d0880a47403160feff8a024725
JohnNathanNeri <shroud546@gmail.com>
2024-09-23 15:15:48 +0800

    change some text

--- bank_loan.py
+++ bank_loan.py
@@ -1,20 +1,21 @@
 # Get user input for salary and loan amount
-salary = float(input("Enter your monthly salary: "))
+salary = int(input("Enter your monthly salary: "))
 loanAmount = float(input("Enter the amount you want to loan: "))
 
-# Check eligibility
+    # Check eligibility
 if salary >= 30000:
     if loanAmount <= (salary * 10):
             # Eligible for loan
-            months = int(input("How many months do you plan to pay back the loan? "))
-            interest = loanAmount * 0.10  # 10% interest
+            months = int(input("How many months will you pay the loan? "))
+            #10% interest
+            interest = loanAmount * 0.10  
             totalAmount = loanAmount + interest
             monthly_payment = totalAmount / months
 
-            print(f"Loan Approved!")
-            print(f"Total Amount to be paid (including interest): {totalAmount:.2f}")
+            print(f"Congrats! Your request for loan has been approved!")
+            print(f"Total Amount you need to pay (including interest): {totalAmount:.2f}")
             print(f"Monthly Payment: {monthly_payment:.2f}")
     else:
             print("Not approved: The loan amount requested exceeds 10 times your monthly salary.")
 else:
-    print("Not approved: Your monthly salary is less than 30,000.00.")
+    print("Not approved: Your poor and your monthly salary is less than 30,000.00.")
